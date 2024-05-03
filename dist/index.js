ca7e62d1985763faf13030601cd40fc8ec978ba5
Caleb Han <calebhan08js@gmail.com>
2024-05-03 22:51:22 +0800

    Added comment explanations

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,21 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 const typeDefs = `
+# schema defines structure of data (type definitions) 
+# also defines queryable fields for every book in our data source
   type Book {
-    title: String
+    title: String 
     author: String
   }
 
+  # lists all available queries executable
+  # e.g. returns array in this case
   type Query {
     books: [Book]
   }
 `;
 const books = [
+    // ur data
     {
         title: "Title 1",
         author: "Author 1",
@@ -22,14 +27,17 @@ const books = [
 ];
 const resolvers = {
     Query: {
-        books: () => books,
+        books: () => books, // retrieves data
     },
 };
+// construct apollo servers
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 });
+// creates express app
+// preps app to handle requests from clients
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+    listen: { port: 4000 }, // specifies port
 });
-console.log(`🚀  Server ready at: ${url}`);
+console.log(`🚀 Server ready at: ${url}`);
