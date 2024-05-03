e561683858a2000cd8217dabc0e822e4f6cd49a5
Caleb Han <calebhan08js@gmail.com>
2024-05-03 22:45:52 +0800

    Added queries and express app

---
+++ dist/index.js
@@ -0,0 +1,35 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+const typeDefs = `
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+const books = [
+    {
+        title: "Title 1",
+        author: "Author 1",
+    },
+    {
+        title: "Title 2",
+        author: "Author 2",
+    },
+];
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at: ${url}`);
